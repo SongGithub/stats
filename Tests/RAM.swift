21b537a9fd42ed34ec82b21e08cda5eeb58e4b8a
song.jin <song.jin@xero.com>
2022-08-06 13:56:13 +1000

    hello word

--- Tests/RAM.swift
+++ Tests/RAM.swift
@@ -11,49 +11,49 @@
 
 import XCTest
 import RAM
-
+  hello world
 class RAM: XCTestCase {
     func testProcessReader_parseProcess() throws {
         var process = ProcessReader.parseProcess("3127  lldb-rpc-server  611M")
         XCTAssertEqual(process.pid, 3127)
         XCTAssertEqual(process.command, "lldb-rpc-server")
         XCTAssertEqual(process.usage, 611 * Double(1024 * 1024))
-        
+
         process = ProcessReader.parseProcess("257   WindowServer     210M")
         XCTAssertEqual(process.pid, 257)
         XCTAssertEqual(process.command, "WindowServer")
         XCTAssertEqual(process.usage, 210 * Double(1024 * 1024))
-        
+
         process = ProcessReader.parseProcess("7752  phpstorm         1819M")
         XCTAssertEqual(process.pid, 7752)
         XCTAssertEqual(process.command, "phpstorm")
         XCTAssertEqual(process.usage, 1819 * Double(1024 * 1024))
-        
+
         process = ProcessReader.parseProcess("359   NotificationCent 62M")
         XCTAssertEqual(process.pid, 359)
         XCTAssertEqual(process.command, "NotificationCe")
         XCTAssertEqual(process.usage, 62 * Double(1024 * 1024))
-        
+
         process = ProcessReader.parseProcess("623    SafariCloudHisto 1608K")
         XCTAssertEqual(process.pid, 623)
         XCTAssertEqual(process.command, "SafariCloudHisto")
         XCTAssertEqual(process.usage, (1608/1024) * Double(1024 * 1024))
-        
+
         process = ProcessReader.parseProcess("174    WindowServer     1442M+ ")
         XCTAssertEqual(process.pid, 174)
         XCTAssertEqual(process.command, "WindowServer")
         XCTAssertEqual(process.usage, 1442 * Double(1024 * 1024))
-        
+
         process = ProcessReader.parseProcess("329    Finder           488M+ ")
         XCTAssertEqual(process.pid, 329)
         XCTAssertEqual(process.command, "Finder")
         XCTAssertEqual(process.usage, 488 * Double(1024 * 1024))
     }
-    
+
     func testReplacePID() throws {
         let raw = "0      kernel_task      270M "
         let process = ProcessReader.parseProcess(raw)
-        
+
         XCTAssertEqual(process.pid, 0)
         XCTAssertEqual(process.command, "kernel_task")
         XCTAssertEqual(process.usage, 270 * Double(1024 * 1024))
